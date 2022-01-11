715504b386697b0467a1abc7a979071e805ff951
yass-dev <yass-dev@users.noreply.github.com>
2022-01-11 13:30:06 +0000

    deploy: fd4638e1e1f6acaec8c1023374459469a2bab264

---
+++ precache-manifest.96139c39fb679d2e78cdba64727575a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6301e1f76cb8a14df57",
+    "url": "/css/app.10c3df4a.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "75f02b4441c0db643520fa90b05875bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cce50c8df269e14f802f",
+    "url": "/js/about.fda73a6c.js"
+  },
+  {
+    "revision": "a6301e1f76cb8a14df57",
+    "url": "/js/app.17a1a6b2.js"
+  },
+  {
+    "revision": "b0dfe3a9835828fc4f1b",
+    "url": "/js/chunk-vendors.09d6974d.js"
+  },
+  {
+    "revision": "aa21e42e01f4edff0aaa25661e2e2c9c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
